a0a2752684ada8d8989d71a7ecc6c40619a289ce
ting <a0909513991@gmail.com>
2023-03-20 14:41:10 +0800

    更改p標籤

--- all.js
+++ all.js
@@ -1,42 +1,58 @@
-let data = [
-  {
-    id: 0,
-    name: "肥宅心碎賞櫻3日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    area: "高雄",
-    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: "貓空纜車雙程票",
-    imgUrl:
-      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台北",
-    description:
-      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: "台中谷關溫泉會1日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台中",
-    description:
-      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
+// let data = [
+//   {
+//     id: 0,
+//     name: "肥宅心碎賞櫻3日",
+//     imgUrl:
+//       "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
+//     area: "高雄",
+//     description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
+//     group: 87,
+//     price: 1400,
+//     rate: 10,
+//   },
+//   {
+//     id: 1,
+//     name: "貓空纜車雙程票",
+//     imgUrl:
+//       "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+//     area: "台北",
+//     description:
+//       "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
+//     group: 99,
+//     price: 240,
+//     rate: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "台中谷關溫泉會1日",
+//     imgUrl:
+//       "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+//     area: "台中",
+//     description:
+//       "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
+//     group: 20,
+//     price: 1765,
+//     rate: 7,
+//   },
+// ];
+
+let data = [];
+function getApi() {
+  axios
+    .get(
+      "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json"
+    )
+    .then(function (response) {
+      data = response.data.data;
+      render();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 const ul = document.querySelector(".ticketCard-area");
+
 function render() {
   let str = "";
   data.forEach(function (item, index) {
@@ -71,8 +87,9 @@ function render() {
   });
   ul.innerHTML = str;
 }
-render();
+getApi();
 
+const searchResult_text = document.querySelector("#searchResult-text");
 const addBtn = document.querySelector(".addTicket-btn");
 const ticketName = document.querySelector("#ticketName");
 const imgUrl = document.querySelector("#ticketImgUrl");
@@ -81,7 +98,6 @@ const ticketNum = document.querySelector("#ticketNum");
 const ticketRate = document.querySelector("#ticketRate");
 const ticketDescription = document.querySelector("#ticketDescription");
 const ticketRegion = document.querySelector("#ticketRegion");
-
 const addTicket_form = document.querySelector(".addTicket-form");
 
 addBtn.addEventListener("click", function (e) {
@@ -117,7 +133,7 @@ addBtn.addEventListener("click", function (e) {
   console.log(obj);
   data.push(obj);
   console.log(data);
-
+  searchResult_text.innerHTML = `本次搜尋共 ${data.length} 筆資料`;
   render();
   //  ticketName.value = "";
   //  imgUrl.value = "";
@@ -130,10 +146,8 @@ addBtn.addEventListener("click", function (e) {
   // 以後請用這個方法！！！！！！
 });
 
-// change 就是當我在表單中選擇了一個新的值，就可以觸發change事件
 const regionSearch = document.querySelector(".regionSearch");
-const selectCategory = document.querySelector("#category-filter");
-const searchResult_text = document.querySelector("#searchResult-text");
+
 regionSearch.addEventListener("change", function (event) {
   let str = "";
   let newData = data.filter(function (item, index, arr) {
